82dbe6cf77c9677c0362cc53700fe2f73d84db9c
clawoo <alin.claudiu.radut@gmail.com>
2011-09-15 23:03:40 +0300

    first commit

---
+++ ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  RootViewController.h
+//  BreakoutCocos2D-x
+//
+//  Created by Clawoo on 9/15/11.
+//  Copyright __MyCompanyName__ 2011. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
